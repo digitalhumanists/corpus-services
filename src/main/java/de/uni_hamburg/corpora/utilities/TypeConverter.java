176737c959d680d289bc94899f0d7c094dbd91b4
sesv009 <daniel.jettka@uni-hamburg.de>
2017-08-15 15:49:16 +0200

    added conversion methods for Document types

--- src/main/java/de/uni_hamburg/corpora/utilities/TypeConverter.java
+++ src/main/java/de/uni_hamburg/corpora/utilities/TypeConverter.java
@@ -8,9 +8,15 @@ package de.uni_hamburg.corpora.utilities;
 
 import java.io.InputStream;
 import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.transform.stream.StreamSource;
 import org.exmaralda.partitureditor.jexmaralda.BasicTranscription;
 import org.exmaralda.partitureditor.jexmaralda.JexmaraldaException;
+import org.jdom.JDOMException;
+import org.jdom.input.DOMBuilder;
+import org.jdom.output.DOMOutputter;
+import org.jdom.output.XMLOutputter;
 import org.xml.sax.SAXException;
 
 /**
@@ -29,15 +35,49 @@ public class TypeConverter {
         return bt.toXML();
     } 
     
-    public static BasicTranscription String2BasicTranscription (String btAsString) throws SAXException, 
-                                                                                          JexmaraldaException{
-        BasicTranscription bt = new BasicTranscription();
-        bt.BasicTranscriptionFromString(btAsString);
-        return bt;
+    public static BasicTranscription String2BasicTranscription (String btAsString){
+        BasicTranscription btResult = null;
+        try {
+            BasicTranscription bt = new BasicTranscription();
+            bt.BasicTranscriptionFromString(btAsString);
+            btResult = bt;
+        } catch (SAXException ex) {
+            Logger.getLogger(TypeConverter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JexmaraldaException ex) {
+            Logger.getLogger(TypeConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return btResult;
     }
     
     public static StreamSource String2StreamSource(String s){
         StreamSource ss = new StreamSource(new StringReader(s));
         return ss;
+    }
+    
+    public static org.jdom.Document W3cDocument2JdomDocument(org.w3c.dom.Document input) {
+        org.jdom.Document jdomDoc = null;        
+        try{
+            DOMBuilder builder = new DOMBuilder();
+            jdomDoc = builder.build(input);
+        } catch(Exception e){
+            Logger.getLogger(TypeConverter.class.getName()).log(Level.SEVERE, null, e);
+        }        
+        return jdomDoc;
+    }
+    
+    public static String JdomDocument2String(org.jdom.Document jdomDocument){
+        return new XMLOutputter().outputString(jdomDocument);
+
+    }
+
+    public static org.w3c.dom.Document JdomDocument2W3cDocument(org.jdom.Document jdomDoc) {        
+        org.w3c.dom.Document w3cDoc = null;        
+        try{
+            DOMOutputter outputter = new DOMOutputter();
+            w3cDoc = outputter.output(jdomDoc);
+        } catch(JDOMException je){
+            Logger.getLogger(TypeConverter.class.getName()).log(Level.SEVERE, null, je);
+        }        
+        return w3cDoc;
     }
 }
