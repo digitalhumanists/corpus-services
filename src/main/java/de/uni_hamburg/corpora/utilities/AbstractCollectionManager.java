db978f95e0a29b9e1023a7fa221d40f2d58e9802
fsnv625 <fsnv625>
2017-09-29 11:06:16 +0200

    added empty classes from Corpus Services diagram as todos

---
+++ src/main/java/de/uni_hamburg/corpora/utilities/AbstractCollectionManager.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.uni_hamburg.corpora.utilities;
+
+/**
+ *
+ * @author fsnv625
+ * Still to do
+ */
+public abstract class AbstractCollectionManager {
+    /**
+     * Input: + fileInSomeFormat: String
+     * 
+     * + getParametersFile():
++ getCorpusStructureSpecification():
++ getAnnotationSpecification():
++ getComaSpecification():
+
++ getComaFile(): String?
+
++ getCMDI(): ?
++ getAllTranscripts(): Collection?
++ getAllAudioFiles(): Collection?
++ getAllVideoFiles(): Collection?
+
++ getAudioLinkForTranscript(): String
++ getVideoLinkForTranscript(): String
+
++ write():
+
++ zipThings...
+
+
+     * 
+     */
+    
+}
---
+++ src/main/java/de/uni_hamburg/corpora/utilities/RepositoryCollectionManager.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.uni_hamburg.corpora.utilities;
+
+/**
+ *
+ * @author fsnv625
+ */
+public class RepositoryCollectionManager extends AbstractCollectionManager{
+    /**
+     * field: + fileSpecification: ?Stream
+     * + getParametersFile():
++ getCorpusStructureSpecification():
++ getAnnotationSpecification():
++ getComaSpecification():
+
++ getComaFile(): String?
+
++ getCMDI(): ?
++ getAllTranscripts(): Collection?
++ getAllAudioFiles(): Collection?
++ getAllVideoFiles(): Collection?
+
++ getAudioLinkForTranscript(): String
++ getVideoLinkForTranscript(): String
+
++ 
+     */
+}
