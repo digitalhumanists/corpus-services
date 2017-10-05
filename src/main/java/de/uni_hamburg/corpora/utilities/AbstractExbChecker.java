08021e714c2f6700f75f39a6a924248428481a69
fsnv625 <fsnv625>
2017-10-05 11:28:08 +0200

    added Abstract Coma and Exb Checker

---
+++ src/main/java/de/uni_hamburg/corpora/utilities/AbstractExbChecker.java
@@ -0,0 +1,16 @@
+/*
+ * A command-line interface for checking corpus files.
+ * 
+ * @author Anne Ferger
+ * @author HZSK
+ * 
+ */
+package de.uni_hamburg.corpora.utilities;
+
+/**
+ *
+ * @author fsnv625
+ */
+public abstract class AbstractExbChecker extends AbstractFileChecker{
+    
+}
