317d55abe80a4e6b98dd96de595588ee81f27046
Anne Ferger <anne.ferger@uni-hamburg.de>
2017-10-05 11:28:08 +0200

    added Abstract Coma and Exb Checker

---
+++ src/main/java/de/uni_hamburg/corpora/utilities/AbstractComaChecker.java
@@ -0,0 +1,20 @@
+/*
+ * A command-line interface for checking corpus files.
+ * 
+ * @author Anne Ferger
+ * @author HZSK
+ * 
+ */
+package de.uni_hamburg.corpora.utilities;
+
+/**
+ *
+ * @author fsnv625
+ */
+public abstract class AbstractComaChecker extends AbstractFileChecker{
+    
+ public void getAllExbsInComa (){
+ //to do
+ }   
+    
+}
