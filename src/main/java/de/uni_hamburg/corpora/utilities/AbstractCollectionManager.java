af81341efbee537cbc38eaea7f38811038b4ce70
fsnv625 <fsnv625>
2017-10-11 11:04:18 +0200

    Added methods to CollectionManager

--- src/main/java/de/uni_hamburg/corpora/utilities/AbstractCollectionManager.java
+++ src/main/java/de/uni_hamburg/corpora/utilities/AbstractCollectionManager.java
@@ -5,36 +5,45 @@
  */
 package de.uni_hamburg.corpora.utilities;
 
+import java.util.Collection;
+
 /**
+ * Still to do
  *
  * @author fsnv625
- * Still to do
  */
 public abstract class AbstractCollectionManager {
-    /**
-     * Input: + fileInSomeFormat: String
-     * 
-     * + getParametersFile():
-+ getCorpusStructureSpecification():
-+ getAnnotationSpecification():
-+ getComaSpecification():
 
-+ getComaFile(): String?
+    //important: That shouldn't be the filepath, but the File itself as a String!!
+    String fileAsString;
 
-+ getCMDI(): ?
-+ getAllTranscripts(): Collection?
-+ getAllAudioFiles(): Collection?
-+ getAllVideoFiles(): Collection?
+    public abstract String getFileAsString(String s);
 
-+ getAudioLinkForTranscript(): String
-+ getVideoLinkForTranscript(): String
+    public abstract String getParametersFileAsString();
 
-+ write():
+    //or should this be an array or something?
+    public abstract String getCorpusStructureSpecificationAsString();
 
-+ zipThings...
+    public abstract String getAnnotationSpecificationAsString();
 
+    //not yet sure if we need the following
+    public abstract String getComaSpecificationAsString();
 
-     * 
-     */
-    
+    public abstract String getComaFileAsString();
+
+    public abstract String getCMDIAsString();
+
+    public abstract Collection getAllTranscripts();
+
+    public abstract Collection getAllAudioFiles();
+
+    public abstract Collection getAllVideoFiles();
+
+    public abstract String getAudioLinkForTranscript();
+
+    public abstract String getVideoLinkForTranscript();
+
+    public abstract void write();
+
+    public abstract void zipThings();
 }
