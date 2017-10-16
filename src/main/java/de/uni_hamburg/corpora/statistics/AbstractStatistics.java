7843ca0c87a9919430eaafad88e9a501a884f6a7
Anne Ferger <anne.ferger@uni-hamburg.de>
2017-10-16 15:07:55 +0200

    added statistics package and to do class

---
+++ src/main/java/de/uni_hamburg/corpora/statistics/AbstractStatistics.java
@@ -0,0 +1,18 @@
+/*
+ * A command-line interface for checking corpus files.
+ * 
+ * @author Anne Ferger
+ * @author HZSK
+ * 
+ */
+package de.uni_hamburg.corpora.statistics;
+
+/**
+ *
+ * @author fsnv625
+ */
+public class AbstractStatistics {
+    
+    //just so we know we still have to implement that
+    
+}
