76e2ccb5d6f2ef152361114c6bc3d704ec7b8f98
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2017-12-14 19:05:05 +0100

    interface for string data checking ok?

---
+++ src/main/java/de/uni_hamburg/corpora/validation/StringChecker.java
@@ -0,0 +1,29 @@
+/**
+ * @file StringChecker.java
+ *
+ * A corpus processor interface that can check raw string data.
+ *
+ * @author Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
+ * @author HZSK
+ */
+
+package de.uni_hamburg.corpora.validation;
+
+/**
+ * A HZSK validator that can check data from a string. Most validators should
+ * start with this. The data is typically file contents as string, but not
+ * necessarily. The validator should create a statistics report while checking.
+ *
+ * @sa de.uni_hamburg.corpora.utilities.TypeConverter
+ *
+ */
+public interface StringChecker {
+
+    /**
+     * Check string data and create a report.
+     */
+    public StatisticsReport check(String data);
+
+
+}
+
