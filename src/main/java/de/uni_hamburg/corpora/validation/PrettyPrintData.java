f6c19ba5df8f150c3e92b7af3f979f2c561573d4
anne <anne.ferger@uni-hamburg.de>
2018-02-21 09:39:53 +0100

    created class to enable PrettyPrint as a fix in validation

---
+++ src/main/java/de/uni_hamburg/corpora/validation/PrettyPrintData.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.uni_hamburg.corpora.validation;
+
+/**
+ *
+ * @author fsnv625
+ */
+public class PrettyPrintData implements Check{
+
+    @Override
+    public void check() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void fix() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
