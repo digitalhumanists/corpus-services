b64081e40cc3ee7d63b479809fd40c79257f5879
anne <anne.ferger@uni-hamburg.de>
2018-03-02 10:50:54 +0100

    more functionality for magician

--- src/main/java/de/uni_hamburg/corpora/CorpusFunction.java
+++ src/main/java/de/uni_hamburg/corpora/CorpusFunction.java
@@ -12,15 +12,18 @@ import java.util.Collection;
  * @author fsnv625
  */
 public interface CorpusFunction extends CommandLineable{
-//Applicability (I already forgot what this was about...)
-
+    
+   
 public Collection<CorpusData> IsUsableFor();
 
-//does CorpusData need to be a field here too?? 
-    
-//WriteFormatBehaviour
-
-//field: WriteLocationBeviour
-
 public Report execute(CorpusData cd);
+
+public Report execute(Corpus c);
+
+public Report execute(Collection<CorpusData> cdc);
+
+public Collection<CorpusData> getIsUsableFor();
+
+public void setIsUsableFor(Collection<CorpusData> cdc);
+
 }
