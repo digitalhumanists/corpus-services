473057d94b43ab19fa454b54ab6e415e556bf9b9
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2018-03-14 03:40:18 +0100

    main for testings

--- src/main/java/de/uni_hamburg/corpora/visualization/HScoreHTML.java
+++ src/main/java/de/uni_hamburg/corpora/visualization/HScoreHTML.java
@@ -11,6 +11,11 @@ import de.uni_hamburg.corpora.utilities.XSLTransformer;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,32 +30,32 @@ import javax.xml.transform.stream.StreamSource;
  * @author Daniel Jettka
  */
 public class HScoreHTML extends AbstractVisualization {
-        
+
     // resources loaded from directory supplied in pom.xml
     private static final String STYLESHEET_PATH = "/xsl/EXB2hScoreHTML.xsl";
     private final String SERVICE_NAME = "HScoreHTML";
-    
-    
+
+
     public HScoreHTML(String btAsString){
         createFromBasicTranscription(btAsString);
     }
-    
-	
+
+
 	 /**
 	 * This method deals performs the transformation of EXB to horizontal Score HTML
 	 *
 	 * @param  btAsString  the EXB file represented in a String object
-	 * @return  
+	 * @return
 	 */
     public void createFromBasicTranscription(String btAsString){
-        
+
         basicTranscriptionString = btAsString;
         basicTranscription = TypeConverter.String2BasicTranscription(btAsString);
-        
+
         String result = null;
-        
+
         try {
-             
+
             String xsl = TypeConverter.InputStream2String(getClass().getResourceAsStream(STYLESHEET_PATH));
 
             // perform XSLT transformation
@@ -59,14 +64,39 @@ public class HScoreHTML extends AbstractVisualization {
             xt.setParameter("WEBSERVICE_NAME", SERVICE_NAME);
             xt.setParameter("HZSK_WEBSITE", HZSK_WEBSITE);
             result = xt.transform(basicTranscriptionString, xsl);
-            
+
 
         } catch (TransformerException ex) {
             Logger.getLogger(HScoreHTML.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
+
         setHTML(result);
     }
-    
-    
+
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                System.out.println("Usage: " + HScoreHTML.class.getName() +
+                        "EXB [HTML]");
+                System.exit(1);
+            } else {
+                byte[] encoded = Files.readAllBytes(Paths.get(args[0]));
+                String btString = new String(encoded, "UTF-8");
+                HScoreHTML score = new HScoreHTML(btString);
+                if (args.length >= 2) {
+                    PrintWriter htmlOut = new PrintWriter(args[1]);
+                    htmlOut.print(score.getHTML());
+                    htmlOut.close();
+                } else {
+                    System.out.println(score.getHTML());
+                }
+            }
+        } catch (UnsupportedEncodingException uee) {
+            uee.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+
 }
