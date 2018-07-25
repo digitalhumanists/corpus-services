abf2ef15d7622b28bc499e9038afaaa58d0d9dd6
7oezdemi <7oezdemi@informatik.uni-hamburg.de>
2018-07-25 13:09:51 +0200

    Problem fixed, dependency added

--- src/main/java/de/uni_hamburg/corpora/conversion/AddCSVMetadataToComa.java
+++ src/main/java/de/uni_hamburg/corpora/conversion/AddCSVMetadataToComa.java
@@ -1,14 +1,12 @@
-package de.uni_hamburg.corpora.utilities;
+package de.uni_hamburg.corpora.conversion;
 
 import com.opencsv.CSVReader;
 import org.exmaralda.partitureditor.jexmaralda.JexmaraldaException;
 import org.xml.sax.SAXException;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
