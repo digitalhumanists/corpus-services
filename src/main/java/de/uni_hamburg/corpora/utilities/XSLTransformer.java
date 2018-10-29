11323ae3f71795d8d19150c67bc81c170c0e17b3
Anne Ferger <fsnv625>
2018-10-29 17:37:33 +0100

    trying to read xsl:messages into the error lists

--- src/main/java/de/uni_hamburg/corpora/utilities/XSLTransformer.java
+++ src/main/java/de/uni_hamburg/corpora/utilities/XSLTransformer.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.uni_hamburg.corpora.utilities;
 
 import java.io.StringWriter;
@@ -11,146 +10,190 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.transform.ErrorListener;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import net.sf.saxon.Controller;
+import net.sf.saxon.event.ReceiverOptions;
+import net.sf.saxon.expr.instruct.TerminationException;
+import net.sf.saxon.serialize.MessageWarner;
+import net.sf.saxon.serialize.XMLEmitter;
+import net.sf.saxon.trans.XPathException;
 
 /**
  *
  * @author Daniel Jettka
  *
- * Class for performing XSLT transformation with net.sf.saxon.TransformerFactoryImpl or other custom implementation.
+ * Class for performing XSLT transformation with
+ * net.sf.saxon.TransformerFactoryImpl or other custom implementation.
  */
 public class XSLTransformer {
-    
+
     private TransformerFactory tranformerFactory;
     private Transformer transformer;
     private String transformerFactoryImpl = "net.sf.saxon.TransformerFactoryImpl";
     private Map<String, Object> parameters = new HashMap<>();
-    
+
     /**
-    * Class constructor.
-    */
-    public XSLTransformer() throws TransformerConfigurationException{
+     * Class constructor.
+     */
+    public XSLTransformer() throws TransformerConfigurationException {
         tranformerFactory = TransformerFactory.newInstance(transformerFactoryImpl, null);
     }
-	
-	/**
-    * Class constructor specifying the TransformerFactoryImpl used for XSLT transformation.
-    */
-    public XSLTransformer(String impl) throws TransformerConfigurationException{
-		transformerFactoryImpl = impl;
+
+    /**
+     * Class constructor specifying the TransformerFactoryImpl used for XSLT
+     * transformation.
+     */
+    public XSLTransformer(String impl) throws TransformerConfigurationException {
+        transformerFactoryImpl = impl;
         tranformerFactory = TransformerFactory.newInstance(transformerFactoryImpl, null);
     }
-    
+
     /**
-	 * Returns a String object that represents the result of an XSLT transformation.
-	 *
-	 * @param  xml  XML as String object that is used as the basis for the XSLT transformation
-	 * @param  xsl  XSLT stylesheet as String object
-	 * @return      the result of the XSLT transformation as String object
-	 */
-    public String transform(String xml, String xsl) throws TransformerException{
+     * Returns a String object that represents the result of an XSLT
+     * transformation.
+     *
+     * @param xml XML as String object that is used as the basis for the XSLT
+     * transformation
+     * @param xsl XSLT stylesheet as String object
+     * @return the result of the XSLT transformation as String object
+     */
+    public String transform(String xml, String xsl) throws TerminationException, TransformerException {
         StreamSource xslSource = TypeConverter.String2StreamSource(xsl);
         StreamSource xmlSource = TypeConverter.String2StreamSource(xml);
         return transform(xmlSource, xslSource);
     }
-    
+
     /**
-	 * Returns a String object that represents the result of an XSLT transformation.
-	 *
-	 * @param  xml  XML as StreamSource object that is used as the basis for the XSLT transformation
-	 * @param  xsl  XSLT stylesheet as StreamSource object
-	 * @return      the result of the XSLT transformation as String object
-	 */
-    public String transform(StreamSource xmlSource, StreamSource xslSource){
-        
+     * Returns a String object that represents the result of an XSLT
+     * transformation.
+     *
+     * @param xml XML as StreamSource object that is used as the basis for the
+     * XSLT transformation
+     * @param xsl XSLT stylesheet as StreamSource object
+     * @return the result of the XSLT transformation as String object
+     */
+    public String transform(StreamSource xmlSource, StreamSource xslSource) throws TransformerConfigurationException, TransformerException, TerminationException {
+        try {
         String result = null;
-        
-        try{
-            transformer = tranformerFactory.newTransformer(xslSource);
 
-            // set the parameters for XSLT transformation
-            for (Map.Entry<String, Object> param : parameters.entrySet()){
-                transformer.setParameter(param.getKey(), param.getValue());
-            }
-
-            //transform and fetch result
-            StringWriter resultWriter = new StringWriter();
-            transformer.transform(xmlSource, new StreamResult(resultWriter));
-            result = resultWriter.toString();
-        } catch (TransformerConfigurationException ex) {
-            Logger.getLogger(XSLTransformer.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (TransformerException ex) {
-            Logger.getLogger(XSLTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        transformer = tranformerFactory.newTransformer(xslSource);
+        // set the parameters for XSLT transformation
+        for (Map.Entry<String, Object> param : parameters.entrySet()) {
+            transformer.setParameter(param.getKey(), param.getValue());
         }
+        transformer = routeMessages(transformer);
+        //transform and fetch result
+        StringWriter resultWriter = new StringWriter();
+        transformer.transform(xmlSource, new StreamResult(resultWriter));
+        result = resultWriter.toString();
 
         return result;
+        }
+        catch (TransformerException e) {
+        System.out.println("Message: " + e.getMessage());
+        throw e;
     }
-    
-	/**
-	 * Set a single parameter for the XSLT transformation.
-	 *
-	 * @param  parameterName   Name of the parameter
-	 * @param  parameterValue  Value of the parameter
-	 * @return      
-	 */
-    public void setParameter(String parameterName, Object parameterValue){        
+    }
+
+    /**
+     * Set a single parameter for the XSLT transformation.
+     *
+     * @param parameterName Name of the parameter
+     * @param parameterValue Value of the parameter
+     * @return
+     */
+    public void setParameter(String parameterName, Object parameterValue) {
         parameters.put(parameterName, parameterValue);
     }
-    
-	/**
-	 * Set a bunch of parameters for the XSLT transformation.
-	 *
-	 * @param  params  Map object representing parameter names and values
-	 * @return      
-	 */
-    public void setParameters(Map<String, Object> params){        
+
+    /**
+     * Set a bunch of parameters for the XSLT transformation.
+     *
+     * @param params Map object representing parameter names and values
+     * @return
+     */
+    public void setParameters(Map<String, Object> params) {
         parameters = params;
     }
-    
-	/**
-	 * Get a single parameter that was set for the XSLT transformation.
-	 *
-	 * @param    parameterName  Name of the parameter that shall be returned
-	 * @return   Value of the supplied parameter
-	 */
-    public Object getParameter(String parameterName){
+
+    /**
+     * Get a single parameter that was set for the XSLT transformation.
+     *
+     * @param parameterName Name of the parameter that shall be returned
+     * @return Value of the supplied parameter
+     */
+    public Object getParameter(String parameterName) {
         return parameters.get(parameterName);
     }
-    
-	/**
-	 * Get all parameters as Map object that were set for the XSLT transformation.
-	 *
-	 * @param    
-	 * @return   Map representing all parameters 
-	 */
-    public Map getParameters(){
+
+    /**
+     * Get all parameters as Map object that were set for the XSLT
+     * transformation.
+     *
+     * @param
+     * @return Map representing all parameters
+     */
+    public Map getParameters() {
         return parameters;
     }
-    
-	/**
-	 * Set TransformerFactoryImpl (represented as class name in String) for the XSLT transformation.
-	 *
-	 * @param  impl  TransformerFactoryImpl represented as class name in String 
-	 * @return   
-	 */
-    public void setTransformerFactoryImpl(String impl){
+
+    /**
+     * Set TransformerFactoryImpl (represented as class name in String) for the
+     * XSLT transformation.
+     *
+     * @param impl TransformerFactoryImpl represented as class name in String
+     * @return
+     */
+    public void setTransformerFactoryImpl(String impl) {
         transformerFactoryImpl = impl;
         tranformerFactory = TransformerFactory.newInstance(transformerFactoryImpl, null);
     }
-    
-	/**
-	 * Get TransformerFactoryImpl (represented as class name in String) for the XSLT transformation.
-	 *
-	 * @param  
-	 * @return   TransformerFactoryImpl represented as class name in String
-	 */
-    public String getTransformerFactoryImpl(){
+
+    /**
+     * Get TransformerFactoryImpl (represented as class name in String) for the
+     * XSLT transformation.
+     *
+     * @param
+     * @return TransformerFactoryImpl represented as class name in String
+     */
+    public String getTransformerFactoryImpl() {
         return transformerFactoryImpl;
     }
-        
+
+    private Transformer routeMessages(Transformer transformer) {
+        if (transformer instanceof net.sf.saxon.jaxp.TransformerImpl) {
+            net.sf.saxon.jaxp.TransformerImpl saxon = (net.sf.saxon.jaxp.TransformerImpl) transformer;
+            Controller controller = saxon.getUnderlyingController();
+            controller.setErrorListener(new ErrorListener() {
+
+                @Override
+                public void warning(TransformerException exception)
+                        throws TransformerException {
+                    System.out.println(exception.getLocalizedMessage());
+                    throw(exception);
+                }
+
+                @Override
+                public void fatalError(TransformerException exception)
+                        throws TransformerException {
+                    warning(exception);
+                }
+
+                @Override
+                public void error(TransformerException exception)
+                        throws TransformerException {
+                    warning(exception);
+                }
+            });
+            controller.setMessageEmitter(new MessageWarner());
+        }
+        return transformer;
+    }
+
 }
