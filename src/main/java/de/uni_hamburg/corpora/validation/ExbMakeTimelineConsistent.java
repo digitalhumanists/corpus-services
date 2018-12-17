3b054b677fbc56089e6c94bebe2e1de86db6c7d4
Anne Ferger <anne.ferger@uni-hamburg.de>
2018-12-17 12:25:53 +0100

    make timeline consitstent

--- src/main/java/de/uni_hamburg/corpora/validation/ExbMakeTimelineConsistent.java
+++ src/main/java/de/uni_hamburg/corpora/validation/ExbMakeTimelineConsistent.java
@@ -5,10 +5,78 @@
  */
 package de.uni_hamburg.corpora.validation;
 
+import de.uni_hamburg.corpora.BasicTranscriptionData;
+import de.uni_hamburg.corpora.CorpusData;
+import de.uni_hamburg.corpora.CorpusFunction;
+import de.uni_hamburg.corpora.CorpusIO;
+import de.uni_hamburg.corpora.Report;
+import java.io.IOException;
+import java.util.Collection;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.exmaralda.partitureditor.jexmaralda.BasicTranscription;
+
 /**
  *
  * @author fsnv625
  */
-public class ExbMakeTimelineConsistent {
+public class ExbMakeTimelineConsistent extends Checker implements CorpusFunction {
+    Document doc = null;
+    BasicTranscriptionData btd = null;
+    Boolean interpolateTimeline = false;
+    String ne = "MakeTimelineConsistent";
     
+    @Override
+    public Report check(CorpusData cd) {
+        report.addCritical(ne, cd.getURL().getFile(), "Checking option is not available");
+        return report;
+    }
+
+    @Override
+    public Report fix(CorpusData cd) {
+        try {
+            btd = (BasicTranscriptionData) cd;
+            BasicTranscription bt = btd.getEXMARaLDAbt();
+            bt.getBody().getCommonTimeline().makeConsistent();
+            if(interpolateTimeline){
+                bt.getBody().getCommonTimeline().completeTimes();
+            }
+            btd.setReadbtasjdom(bt.toJDOMDocument());
+            btd.setOriginalString(bt.toXML());
+            //btd.updateReadbtasjdom();
+            cd = (CorpusData) btd;
+            CorpusIO cio = new CorpusIO();
+            cio.write(cd, cd.getURL());
+            if(cd != null){
+            report.addCorrect(ne, cd, "made timeline consistent");   
+            }
+            else{
+            report.addCritical(ne, cd, "making timeline consistent not possible");
+            }
+        } catch (JDOMException ex) {
+            report.addException(ex, ne, cd, "unknown xml exception");
+        } catch (IOException ex) {
+            report.addException(ex, ne, cd, "unknown IO exception");
+        }
+        return report;
+    }
+
+    @Override
+    public Collection<Class<? extends CorpusData>> getIsUsableFor() {
+        try {
+            Class cl = Class.forName("de.uni_hamburg.corpora.BasicTranscriptionData");
+            IsUsableFor.add(cl);
+
+        } catch (ClassNotFoundException ex) {
+            report.addException(ex, "unknown class not found error");
+        }
+        return IsUsableFor;
+    }
+    
+    public void setInterpolateTimeline(String s){
+        interpolateTimeline = false;
+        if (s.equals("true") || s.equals("wahr") || s.equals("ja") || s.equals("yes")) {
+            interpolateTimeline = true;
+        }
+    }
 }
