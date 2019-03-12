16bbd2bcf6dfd51e2d95a3fb81090efe51fd0fa7
7oezdemi <7oezdemi@informatik.uni-hamburg.de>
2019-03-12 13:29:44 +0100

    Made the exb structure checker compatible with the new structure and added a test for it
    Former-commit-id: 5afec4439721b5aebec7a518da543cf27127f667

---
+++ src/test/java/de/uni_hamburg/corpora/validation/ExbStructureCheckerTest.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.uni_hamburg.corpora.validation;
+
+import de.uni_hamburg.corpora.Corpus;
+import de.uni_hamburg.corpora.CorpusData;
+import de.uni_hamburg.corpora.Report;
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ozzy
+ */
+public class ExbStructureCheckerTest {
+
+    public ExbStructureCheckerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of check method, of class ComaXsdChecker.
+     */
+    @Test
+    public void testCheck() throws Exception {
+        System.out.println("check");
+        String corpusFolder = "src/test/java/de/uni_hamburg/corpora/resources/example";
+        URL corpusURL = Paths.get(corpusFolder).toUri().toURL();
+        Corpus corp = new Corpus(corpusURL);
+        ExbStructureChecker instance = new ExbStructureChecker();
+        instance.report = new Report();
+        Collection<CorpusData> cdc;
+        //what happens when we check exb files
+        for (CorpusData cd : corp.getContentdata()) {
+            assertNotNull(instance.check(cd));
+        }
+    }
+
+    /**
+     * Test of getIsUsableFor method, of class ComaXsdChecker.
+     */
+    @Test
+    public void testGetIsUsableFor() {
+        System.out.println("getIsUsableFor");
+        ExbStructureChecker instance = new ExbStructureChecker();
+        //Collection<Class> expResult = null;
+        Collection<Class<? extends CorpusData>> result = instance.getIsUsableFor();
+        //no null object here
+        assertNotNull(result);
+    }
+
+}
