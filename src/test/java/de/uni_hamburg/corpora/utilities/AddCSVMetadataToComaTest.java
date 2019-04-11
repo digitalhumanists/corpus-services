ed90bcc5629cb3d060da1cd553ae6951bf5c25ce
7oezdemi <7oezdemi@informatik.uni-hamburg.de>
2019-04-11 13:47:43 +0200

    Missing methods and their tests added.
    Former-commit-id: 0e424e6991ec3e1f168be3ff2a1b76e2419703d5

--- src/test/java/de/uni_hamburg/corpora/utilities/AddCSVMetadataToComaTest.java
+++ src/test/java/de/uni_hamburg/corpora/utilities/AddCSVMetadataToComaTest.java
@@ -6,9 +6,17 @@
 package de.uni_hamburg.corpora.utilities;
 
 import com.opencsv.CSVReader;
+import de.uni_hamburg.corpora.Corpus;
+import de.uni_hamburg.corpora.CorpusData;
+import de.uni_hamburg.corpora.Report;
+import de.uni_hamburg.corpora.conversion.AddCSVMetadataToComa;
+import de.uni_hamburg.corpora.validation.ExbSegmentationChecker;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.List;
 import org.jdom.JDOMException;
 import org.junit.After;
@@ -59,6 +67,39 @@ public class AddCSVMetadataToComaTest {
         AddCSVMetadataToComa instance = new AddCSVMetadataToComa(comaFile, csvFile, isSpeaker);
         instance.inputData();
 */
+    }
+        /**
+     * Test of check method, of class AddCSVMetadataToComa.
+     */
+    @Test
+    public void testCheck() throws Exception {
+        System.out.println("check");
+        String corpusFolder = "src/test/java/de/uni_hamburg/corpora/resources/example";
+        URL corpusURL = Paths.get(corpusFolder).toUri().toURL();
+        Corpus corp = new Corpus(corpusURL);
+        AddCSVMetadataToComa instance = new AddCSVMetadataToComa();
+        instance.setCSVFilePath("C:\\Users\\Ozzy\\HZSK\\hzsk-corpus-services\\src"
+                + "\\test\\java\\de\\uni_hamburg\\corpora\\resources\\example\\sample-csv.csv");
+        instance.setSpeakerOrCommunication("speaker");
+        //instance.report = new Report();
+        Collection<CorpusData> cdc;
+        //what happens when we check exb files
+        for (CorpusData cd : corp.getMetadata()) {
+            assertNotNull(instance.check(cd));
+        }
+    }
+
+    /**
+     * Test of getIsUsableFor method, of class AddCSVMetadataToComa.
+     */
+    @Test
+    public void testGetIsUsableFor() {
+        System.out.println("getIsUsableFor");
+        AddCSVMetadataToComa instance = new AddCSVMetadataToComa();
+        //Collection<Class> expResult = null;
+        Collection<Class<? extends CorpusData>> result = instance.getIsUsableFor();
+        //no null object here
+        assertNotNull(result);
     }
     
 }
