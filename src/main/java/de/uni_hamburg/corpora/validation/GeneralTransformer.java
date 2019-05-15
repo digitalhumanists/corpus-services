77a44c0b60cf5ba6454d8bd2355bd8be400f317b
Anne Ferger <fsnv625>
2019-05-15 14:05:11 +0200

    general transformer working with some parameters
    Former-commit-id: aa02f019d19612ac60d53f8d1d8eeb5f6420732b

--- src/main/java/de/uni_hamburg/corpora/validation/GeneralTransformer.java
+++ src/main/java/de/uni_hamburg/corpora/validation/GeneralTransformer.java
@@ -9,8 +9,8 @@ import de.uni_hamburg.corpora.Report;
 import de.uni_hamburg.corpora.utilities.XSLTransformer;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,6 +20,9 @@ import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
 import org.exmaralda.partitureditor.jexmaralda.JexmaraldaException;
 import org.jdom.JDOMException;
 import org.xml.sax.SAXException;
+import static de.uni_hamburg.corpora.utilities.PrettyPrinter.indent;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
 
 /**
  *
@@ -28,9 +31,13 @@ import org.xml.sax.SAXException;
 public class GeneralTransformer extends Checker {
 
     String pathToXSL = "";
-    String urlToOutput;
+    String outputFilename = "";
+    URL urlToOutput;
     String getra = "GeneralTransformer";
     boolean overwritefiles = false;
+    boolean coma = false;
+    boolean exb = false;
+    boolean exs = false;
 
     @Override
     public Report check(CorpusData cd) throws SAXException, JexmaraldaException {
@@ -40,17 +47,19 @@ public class GeneralTransformer extends Checker {
     }
 
     @Override
-    public Report fix(CorpusData cd) throws SAXException, JDOMException, IOException, JexmaraldaException {
+    public Report fix(CorpusData cd) {
         try {
             CorpusIO cio = new CorpusIO();
             String corpusdata = cd.toUnformattedString();
-            String stylesheet = cio.readInternalResourceAsString(pathToXSL);
+            //String stylesheet = cio.readInternalResourceAsString(pathToXSL);
+            String stylesheet = cio.readExternalResourceAsString(pathToXSL);
             XSLTransformer xslt = new XSLTransformer();
             String result
                     = xslt.transform(corpusdata, stylesheet);
             if (result != null) {
                 report.addCorrect(getra, cd.getURL().toString(),
                         "XSL Transformation was successful");
+                result = indent(result, "event");
             }
             if (overwritefiles){
             cio.write(result, cd.getURL());    
@@ -61,6 +70,18 @@ public class GeneralTransformer extends Checker {
             Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
         } catch (TransformerException ex) {
             Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JDOMException ex) {
+            Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (XPathExpressionException ex) {
+            Logger.getLogger(GeneralTransformer.class.getName()).log(Level.SEVERE, null, ex);
         }
         return report;
     }
@@ -68,12 +89,18 @@ public class GeneralTransformer extends Checker {
     @Override
     public Collection<Class<? extends CorpusData>> getIsUsableFor() {
         try {
+            if(exb){
             Class cl = Class.forName("de.uni_hamburg.corpora.BasicTranscriptionData");
             IsUsableFor.add(cl);
+            }
+            if(exs){
             Class cl2 = Class.forName("de.uni_hamburg.corpora.UnspecifiedXMLData");
             IsUsableFor.add(cl2);
+            }
+            if(coma){
             Class cl3 = Class.forName("de.uni_hamburg.corpora.ComaData");
             IsUsableFor.add(cl3);
+            }
         } catch (ClassNotFoundException ex) {
             report.addException(ex, " usable class not found");
         }
@@ -84,12 +111,8 @@ public class GeneralTransformer extends Checker {
         pathToXSL = s;
     }
 
-    public void setUrlToOutput(String s) throws MalformedURLException {
-        if (s.startsWith("file://")) {
-            urlToOutput = new URL(s);
-        } else {
-            urlToOutput = Paths.get(s).toUri().toURL();
-        }
+    public void setOutputFileName(String s) throws MalformedURLException {
+            urlToOutput = new URL(cd.getParentURL() + s);
     }
 
     public void setOverwriteFiles(String s) {
@@ -97,6 +120,36 @@ public class GeneralTransformer extends Checker {
             overwritefiles = true;
         } else if (s.equalsIgnoreCase("false") || s.equalsIgnoreCase("falsch") || s.equalsIgnoreCase("nein")) {
             overwritefiles = false;
+        } else {
+            report.addCritical(getra, cd, "Parameter coma not recognized: " + escapeHtml4(s));
+        }
+    }
+    
+    public void setComa(String s) {
+        if (s.equalsIgnoreCase("true") || s.equalsIgnoreCase("wahr") || s.equalsIgnoreCase("ja")) {
+            coma = true;
+        } else if (s.equalsIgnoreCase("false") || s.equalsIgnoreCase("falsch") || s.equalsIgnoreCase("nein")) {
+            coma = false;
+        } else {
+            report.addCritical(getra, cd, "Parameter coma not recognized: " + escapeHtml4(s));
+        }
+    }
+    
+    public void setExb(String s) {
+        if (s.equalsIgnoreCase("true") || s.equalsIgnoreCase("wahr") || s.equalsIgnoreCase("ja")) {
+            exb = true;
+        } else if (s.equalsIgnoreCase("false") || s.equalsIgnoreCase("falsch") || s.equalsIgnoreCase("nein")) {
+            exb = false;
+        } else {
+            report.addCritical(getra, cd, "Parameter coma not recognized: " + escapeHtml4(s));
+        }
+    }
+    
+    public void setExs(String s) {
+        if (s.equalsIgnoreCase("true") || s.equalsIgnoreCase("wahr") || s.equalsIgnoreCase("ja")) {
+            exs = true;
+        } else if (s.equalsIgnoreCase("false") || s.equalsIgnoreCase("falsch") || s.equalsIgnoreCase("nein")) {
+            exs = false;
         } else {
             report.addCritical(getra, cd, "Parameter coma not recognized: " + escapeHtml4(s));
         }
