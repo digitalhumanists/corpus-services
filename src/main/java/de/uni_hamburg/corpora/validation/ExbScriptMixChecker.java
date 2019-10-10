7c4f1fbad256e199160549ed4005ca5d66ad840b
Timofey Arkhangelskiy <timofey.arkhangelskiy@uni-hamburg.de>
2019-10-10 12:17:21 +0200

    Added getDescription method to ExbScriptMixChecker.
    Former-commit-id: 084c202289d9343cb7267c5f3fb3bb50b3d6098c

--- src/main/java/de/uni_hamburg/corpora/validation/ExbScriptMixChecker.java
+++ src/main/java/de/uni_hamburg/corpora/validation/ExbScriptMixChecker.java
@@ -198,4 +198,9 @@ public class ExbScriptMixChecker extends Checker implements CorpusFunction {
         }
         return stats;
     }
+    
+    @Override
+    public String getDescription() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
