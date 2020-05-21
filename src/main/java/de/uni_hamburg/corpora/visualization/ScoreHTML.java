edc5eb5b20e60c82acaf5d6b6d621ab8f8846c7a
fsnv625 <fsnv625@uni-hamburg.de>
2020-05-21 11:35:49 +0200

    adapted other modules to new structure
    Former-commit-id: 84bfa03c31af75e57dfa6ff0586c46d7f477526b

--- src/main/java/de/uni_hamburg/corpora/visualization/ScoreHTML.java
+++ src/main/java/de/uni_hamburg/corpora/visualization/ScoreHTML.java
@@ -5,6 +5,7 @@
  */
 package de.uni_hamburg.corpora.visualization;
 
+import de.uni_hamburg.corpora.BasicTranscriptionData;
 import de.uni_hamburg.corpora.Corpus;
 import de.uni_hamburg.corpora.CorpusData;
 import de.uni_hamburg.corpora.CorpusIO;
@@ -22,9 +23,12 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 import org.exmaralda.common.corpusbuild.FileIO;
@@ -54,17 +58,16 @@ public class ScoreHTML extends Visualizer {
     private static final String STYLESHEET_PATH = "/xsl/Score2HTML.xsl";
     private final String SERVICE_NAME = "ScoreHTML";
     private Integer width = 900;
-    Report stats;
+    Report stats = new Report();
     URL targeturl;
     CorpusData cd;
     String corpusname = "";
 
     public ScoreHTML() {
-        super("ScoreHTML");
     }
 
-    public ScoreHTML(String btAsString) {
-        super("ScoreHTML");
+    public ScoreHTML(String btAsString) throws JDOMException, TransformerException, TransformerConfigurationException, IOException {
+
         createFromBasicTranscription(btAsString);
     }
 
@@ -75,132 +78,122 @@ public class ScoreHTML extends Visualizer {
      * @param btAsString the EXB file represented in a String object
      * @return
      */
-    public String createFromBasicTranscription(String btAsString) {
+    public String createFromBasicTranscription(String btAsString) throws JDOMException, TransformerConfigurationException, TransformerException, IOException {
 
         basicTranscriptionString = btAsString;
         basicTranscription = TypeConverter.String2BasicTranscription(btAsString);
 
         String result = null;
 
-        try {
+        BasicTranscription bt = basicTranscription;
+        bt.normalize();
 
-            BasicTranscription bt = basicTranscription;
-            bt.normalize();
+        TierFormatTable tft = new TierFormatTable(bt);
 
-            TierFormatTable tft = new TierFormatTable(bt);
+        ItConverter itc = new ItConverter();
+        InterlinearText it = itc.BasicTranscriptionToInterlinearText(bt, tft, 0);
 
-            ItConverter itc = new ItConverter();
-            InterlinearText it = itc.BasicTranscriptionToInterlinearText(bt, tft, 0);
+        //setting HTML parameters
+        HTMLParameters param = new HTMLParameters();
+        param.setWidth(width);
+        param.stretchFactor = 1.2;
+        param.smoothRightBoundary = true;
+        param.includeSyncPoints = true;
+        param.putSyncPointsOutside = true;
+        param.outputAnchors = true;
+        param.frame = "lrtb";
+        param.frameStyle = "Solid";
+        param.setFrameColor(new java.awt.Color(153, 153, 153));
 
-            //setting HTML parameters
-            HTMLParameters param = new HTMLParameters();
-            param.setWidth(width);
-            param.stretchFactor = 1.2;
-            param.smoothRightBoundary = true;
-            param.includeSyncPoints = true;
-            param.putSyncPointsOutside = true;
-            param.outputAnchors = true;
-            param.frame = "lrtb";
-            param.frameStyle = "Solid";
-            param.setFrameColor(new java.awt.Color(153, 153, 153));
+        it.trim(param);
 
-            it.trim(param);
-
-            String itAsString = it.toXML();
-            String styles = "/* EMTPY TIER FORMAT TABLE!!! */";
-            if (bt.getTierFormatTable() != null) {
-                styles = bt.getTierFormatTable().toTDCSS();
-            }
-
-            final Document itDocument = FileIO.readDocumentFromString(itAsString);
-            Document btDocument = bt.toJDOMDocument();
-
-            // remove "line" elements (die stoeren nur)
-            Iterator i = itDocument.getRootElement().getDescendants(new ElementFilter("line"));
-            Vector toBeRemoved = new Vector();
-            while (i.hasNext()) {
-                toBeRemoved.addElement(i.next());
-            }
-            for (int pos = 0; pos < toBeRemoved.size(); pos++) {
-                Element e = (Element) (toBeRemoved.elementAt(pos));
-                e.detach();
-            }
-
-            XPath xpath1 = XPath.newInstance("//common-timeline");
-            Element timeline = (Element) (xpath1.selectSingleNode(btDocument));
-            timeline.detach();
-
-            XPath xpath2 = XPath.newInstance("//head");
-            Element head = (Element) (xpath2.selectSingleNode(btDocument));
-            head.detach();
-
-            XPath xpath3 = XPath.newInstance("//tier");
-            List tiers = xpath3.selectNodes(btDocument);
-            Element tiersElement = new Element("tiers");
-            for (int pos = 0; pos < tiers.size(); pos++) {
-                Element t = (Element) (tiers.get(pos));
-                t.detach();
-                t.removeContent();
-                tiersElement.addContent(t);
-            }
-
-            Element tableWidthElement = new Element("table-width");
-            tableWidthElement.setAttribute("table-width", Long.toString(Math.round(param.getWidth())));
-
-            Element btElement = new Element("basic-transcription");
-            //            btElement.addContent(nameElement);
-            btElement.addContent(tableWidthElement);
-            btElement.addContent(head);
-            btElement.addContent(timeline);
-            btElement.addContent(tiersElement);
-
-            itDocument.getRootElement().addContent(btElement);
-
-            XMLOutputter xmOut = new XMLOutputter();
-            String xml = xmOut.outputString(itDocument);
-
-            String xsl = TypeConverter.InputStream2String(getClass().getResourceAsStream(STYLESHEET_PATH));
-
-            XSLTransformer xt = new XSLTransformer();
-            xt.setParameter("EMAIL_ADDRESS", EMAIL_ADDRESS);
-            xt.setParameter("WEBSERVICE_NAME", SERVICE_NAME);
-            xt.setParameter("HZSK_WEBSITE", HZSK_WEBSITE);
-            xt.setParameter("STYLES", styles);
-            xt.setParameter("TRANSCRIPTION_NAME", cd.getFilenameWithoutFileEnding());
-            if (!corpusname.equals("")) {
-                xt.setParameter("CORPUS_NAME", corpusname);
-            }
-
-            // perform XSLT transformation
-            result = xt.transform(xml, xsl);
-
-            // insert JavaScript for highlighting
-            // replace JS/CSS placeholders from XSLT output
-            try {
-                Pattern regex = Pattern.compile("(<hzsk\\-pi:include( xmlns:hzsk\\-pi=\"https://corpora\\.uni\\-hamburg\\.de/hzsk/xmlns/processing\\-instruction\")?>([^<]+)</hzsk\\-pi:include>)", Pattern.DOTALL);
-                Matcher m = regex.matcher(result);
-                StringBuffer sb = new StringBuffer();
-                while (m.find()) {
-                    String insertion = TypeConverter.InputStream2String(getClass().getResourceAsStream(m.group(3)));
-                    m.appendReplacement(sb, m.group(0).replaceFirst(Pattern.quote(m.group(1)), insertion));
-                }
-                m.appendTail(sb);
-                result = sb.toString();
-            } catch (Exception e) {
-                setHTML("Custom Exception for inserting JS/CSS into result: " + e.getLocalizedMessage() + "\n" + result);
-                stats.addException(e, SERVICE_NAME, cd, "Custom Exception for inserting JS/CSS into result");
-            }
-
-            String js = TypeConverter.InputStream2String(getClass().getResourceAsStream(JS_HIGHLIGHTING_PATH));
-            result = result.replace("<!--jsholder-->", js);
-
-        } catch (IOException ex) {
-            stats.addException(ex, SERVICE_NAME, cd, "Input Output Exception");
-        } catch (TransformerException ex) {
-            stats.addException(ex, SERVICE_NAME, cd, "Transformer Exception");
-        } catch (JDOMException ex) {
-            stats.addException(ex, SERVICE_NAME, cd, "Jdom Exception");
+        String itAsString = it.toXML();
+        String styles = "/* EMTPY TIER FORMAT TABLE!!! */";
+        if (bt.getTierFormatTable() != null) {
+            styles = bt.getTierFormatTable().toTDCSS();
         }
+
+        final Document itDocument = FileIO.readDocumentFromString(itAsString);
+        Document btDocument = bt.toJDOMDocument();
+
+        // remove "line" elements (die stoeren nur)
+        Iterator i = itDocument.getRootElement().getDescendants(new ElementFilter("line"));
+        Vector toBeRemoved = new Vector();
+        while (i.hasNext()) {
+            toBeRemoved.addElement(i.next());
+        }
+        for (int pos = 0; pos < toBeRemoved.size(); pos++) {
+            Element e = (Element) (toBeRemoved.elementAt(pos));
+            e.detach();
+        }
+
+        XPath xpath1 = XPath.newInstance("//common-timeline");
+        Element timeline = (Element) (xpath1.selectSingleNode(btDocument));
+        timeline.detach();
+
+        XPath xpath2 = XPath.newInstance("//head");
+        Element head = (Element) (xpath2.selectSingleNode(btDocument));
+        head.detach();
+
+        XPath xpath3 = XPath.newInstance("//tier");
+        List tiers = xpath3.selectNodes(btDocument);
+        Element tiersElement = new Element("tiers");
+        for (int pos = 0; pos < tiers.size(); pos++) {
+            Element t = (Element) (tiers.get(pos));
+            t.detach();
+            t.removeContent();
+            tiersElement.addContent(t);
+        }
+
+        Element tableWidthElement = new Element("table-width");
+        tableWidthElement.setAttribute("table-width", Long.toString(Math.round(param.getWidth())));
+
+        Element btElement = new Element("basic-transcription");
+        //            btElement.addContent(nameElement);
+        btElement.addContent(tableWidthElement);
+        btElement.addContent(head);
+        btElement.addContent(timeline);
+        btElement.addContent(tiersElement);
+
+        itDocument.getRootElement().addContent(btElement);
+
+        XMLOutputter xmOut = new XMLOutputter();
+        String xml = xmOut.outputString(itDocument);
+
+        String xsl = TypeConverter.InputStream2String(getClass().getResourceAsStream(STYLESHEET_PATH));
+
+        XSLTransformer xt = new XSLTransformer();
+        xt.setParameter("EMAIL_ADDRESS", EMAIL_ADDRESS);
+        xt.setParameter("WEBSERVICE_NAME", SERVICE_NAME);
+        xt.setParameter("HZSK_WEBSITE", HZSK_WEBSITE);
+        xt.setParameter("STYLES", styles);
+        xt.setParameter("TRANSCRIPTION_NAME", cd.getFilenameWithoutFileEnding());
+        if (!corpusname.equals("")) {
+            xt.setParameter("CORPUS_NAME", corpusname);
+        }
+
+        // perform XSLT transformation
+        result = xt.transform(xml, xsl);
+
+        // insert JavaScript for highlighting
+        // replace JS/CSS placeholders from XSLT output
+        try {
+            Pattern regex = Pattern.compile("(<hzsk\\-pi:include( xmlns:hzsk\\-pi=\"https://corpora\\.uni\\-hamburg\\.de/hzsk/xmlns/processing\\-instruction\")?>([^<]+)</hzsk\\-pi:include>)", Pattern.DOTALL);
+            Matcher m = regex.matcher(result);
+            StringBuffer sb = new StringBuffer();
+            while (m.find()) {
+                String insertion = TypeConverter.InputStream2String(getClass().getResourceAsStream(m.group(3)));
+                m.appendReplacement(sb, m.group(0).replaceFirst(Pattern.quote(m.group(1)), insertion));
+            }
+            m.appendTail(sb);
+            result = sb.toString();
+        } catch (Exception e) {
+            setHTML("Custom Exception for inserting JS/CSS into result: " + e.getLocalizedMessage() + "\n" + result);
+            stats.addException(e, SERVICE_NAME, cd, "Custom Exception for inserting JS/CSS into result");
+        }
+
+        String js = TypeConverter.InputStream2String(getClass().getResourceAsStream(JS_HIGHLIGHTING_PATH));
+        result = result.replace("<!--jsholder-->", js);
 
         setHTML(result);
 
@@ -235,27 +228,23 @@ public class ScoreHTML extends Visualizer {
     }
 
     @Override
-    public Report visualize(CorpusData cod) {
-        try {
-            cd = cod;
-            stats = new Report();
-            String result = createFromBasicTranscription(cd.toSaveableString());
-            targeturl = new URL(cd.getParentURL() + cd.getFilenameWithoutFileEnding() + "_score.html");
-            CorpusIO cio = new CorpusIO();
-            cio.write(result, targeturl);
-            stats.addCorrect(SERVICE_NAME, cd, "Visualization of file was successfully saved at " + targeturl);
-        } catch (MalformedURLException ex) {
-            stats.addException(SERVICE_NAME, ex, "Malformed URL used");
-        } catch (IOException ex) {
-            stats.addException(SERVICE_NAME, ex, "Input Output Exception");
-        } catch (TransformerException ex) {
-            stats.addException(SERVICE_NAME, ex, "Transformer Exception");
-        } catch (ParserConfigurationException ex) {
-            stats.addException(SERVICE_NAME, ex, "Parser Exception");
-        } catch (SAXException ex) {
-            stats.addException(SERVICE_NAME, ex, "XML Exception");
-        } catch (XPathExpressionException ex) {
-            stats.addException(SERVICE_NAME, ex, "XPath Exception");
+    public Report function(CorpusData cod) throws JDOMException, TransformerException, MalformedURLException, TransformerConfigurationException, IOException, ParserConfigurationException, SAXException, XPathExpressionException {
+        cd = cod;
+        stats = new Report();
+        String result = createFromBasicTranscription(cd.toSaveableString());
+        targeturl = new URL(cd.getParentURL() + cd.getFilenameWithoutFileEnding() + "_score.html");
+        CorpusIO cio = new CorpusIO();
+        cio.write(result, targeturl);
+        stats.addCorrect(SERVICE_NAME, cd, "Visualization of file was successfully saved at " + targeturl);
+        return stats;
+    }
+
+    @Override
+    public Report function(Corpus co) throws JDOMException, TransformerException, TransformerConfigurationException, IOException, MalformedURLException, ParserConfigurationException, SAXException, XPathExpressionException {
+
+        Collection<BasicTranscriptionData> btc = co.getBasicTranscriptionData();
+        for (BasicTranscriptionData bt : btc) {
+            stats.merge(function(bt));
         }
         return stats;
     }
@@ -293,6 +282,10 @@ public class ScoreHTML extends Visualizer {
             stats.addException(SERVICE_NAME, uee, "encoding exception");
         } catch (IOException ioe) {
             stats.addException(SERVICE_NAME, ioe, "input output exception");
+        } catch (JDOMException ex) {
+            Logger.getLogger(ScoreHTML.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(ScoreHTML.class.getName()).log(Level.SEVERE, null, ex);
         }
         return stats;
     }
@@ -310,10 +303,5 @@ public class ScoreHTML extends Visualizer {
         String description = "This class creates an html visualization "
                 + "in the Score format from an exb. ";
         return description;
-    }
-
-    @Override
-    public Report execute(Corpus c, boolean fix) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
