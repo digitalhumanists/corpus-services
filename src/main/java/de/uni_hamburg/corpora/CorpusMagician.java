14b82e37ab46b2291f3b827cd644cefd780c05d6
Anne Ferger <anne.ferger@uni-hamburg.de>
2020-05-26 16:00:23 +0200

    Revert "fixed a small but very important bug in calling the function on the corpus object"
    This reverts commit 3ee9699accc2158506ddbd1423a734856149f990 [formerly 4bb501df0e56c697f91ccd6bd8d89e521d0fe435].
    
    
    Former-commit-id: 064c3944855542c1ef0cd5e1b2fdc499b05ae12d

--- src/main/java/de/uni_hamburg/corpora/CorpusMagician.java
+++ src/main/java/de/uni_hamburg/corpora/CorpusMagician.java
@@ -859,7 +859,17 @@ public class CorpusMagician {
         //find out on which objects this corpus function can run
         //choose those from the corpus
         //and run the checks on those files recursively
-        cf.execute(c, fix);
+        Collection<Class<? extends CorpusData>> usableTypes = cf.getIsUsableFor();
+
+        //if the corpus files are an instance
+        //of the class cl, run the function
+        for (CorpusData cd : c.getCorpusData()) {
+            if (usableTypes.contains(cd.getClass())) {
+                Report newReport = runCorpusFunction(cd, cf, fix);
+                report.merge(newReport);
+            }
+
+        }
         return report;
     }
 
