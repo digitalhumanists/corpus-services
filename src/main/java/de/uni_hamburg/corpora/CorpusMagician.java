f4cf14f27bb99b6ab76471eec195259d155508fb
Daniel Jettka <sesv009@uni-hamburg.de>
2020-05-26 18:37:58 +0200

    re-doing the important change in CorpusMagician to process Corpus instead of CorpusData, cf. bbc064d9
    Former-commit-id: db90749d0c69192cf4153f961c2de41ceefaf6d5

--- src/main/java/de/uni_hamburg/corpora/CorpusMagician.java
+++ src/main/java/de/uni_hamburg/corpora/CorpusMagician.java
@@ -877,17 +877,8 @@ public class CorpusMagician {
         //find out on which objects this corpus function can run
         //choose those from the corpus
         //and run the checks on those files recursively
-        Collection<Class<? extends CorpusData>> usableTypes = cf.getIsUsableFor();
+        cf.execute(c, fix);
 
-        //if the corpus files are an instance
-        //of the class cl, run the function
-        for (CorpusData cd : c.getCorpusData()) {
-            if (usableTypes.contains(cd.getClass())) {
-                Report newReport = runCorpusFunction(cd, cf, fix);
-                report.merge(newReport);
-            }
-
-        }
         return report;
     }
 
