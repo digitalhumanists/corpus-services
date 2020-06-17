7c2d2f0ece153de90aaaaefac5e56cd44599a70e
Anne Ferger <anne.ferger@uni-hamburg.de>
2020-06-17 15:24:42 +0200

    new vikus viewer files
    Former-commit-id: 7a3b75430cb21e012a789b7ce9359e9101f5e3c3

--- src/main/java/de/uni_hamburg/corpora/visualization/VikusViewer.java
+++ src/main/java/de/uni_hamburg/corpora/visualization/VikusViewer.java
@@ -19,17 +19,42 @@ import org.exmaralda.partitureditor.fsm.FSMException;
 import org.exmaralda.partitureditor.jexmaralda.JexmaraldaException;
 import org.jdom.JDOMException;
 import org.xml.sax.SAXException;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
 
 /**
  *
  * @author anne
  */
-public class VikusViewer  extends Visualizer{
+public class VikusViewer extends Visualizer {
+    
+    private static final String CONFIG_PATH = "/vikus-viewer/config.json";
+    private static final String DATA_PATH = "/vikus-viewer/data.csv";
+    private static final String INFO_PATH = "/vikus-viewer/info.md";
+    private static final String TIMELINE_PATH = "/vikus-viewer/timeline.csv";
 
     @Override
     public Report function(CorpusData cd) throws NoSuchAlgorithmException, ClassNotFoundException, FSMException, URISyntaxException, SAXException, IOException, ParserConfigurationException, JexmaraldaException, TransformerException, XPathExpressionException, JDOMException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    public void createDataCSV(){
+        JSONParser parser = new JSONParser();
+        String s = CONFIG_PATH;
+        
+    }
+    
+   public void createConfigJSON(){
+       
+        
+    }
+        
+    public void createInfoMD(){
+        
+    }
+        
 
     @Override
     public Report function(Corpus c) throws NoSuchAlgorithmException, ClassNotFoundException, FSMException, URISyntaxException, SAXException, IOException, ParserConfigurationException, JexmaraldaException, TransformerException, XPathExpressionException, JDOMException {
@@ -38,12 +63,20 @@ public class VikusViewer  extends Visualizer{
 
     @Override
     public Collection<Class<? extends CorpusData>> getIsUsableFor() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Class cl = Class.forName("de.uni_hamburg.corpora.ComaData");
+            IsUsableFor.add(cl);
+        } catch (ClassNotFoundException ex) {
+            report.addException(ex, "Usable class not found.");
+        }
+        return IsUsableFor;
     }
 
     @Override
     public String getDescription() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String description = "This class creates an config files needed "
+                + "for the vikus-viewer software. ";
+        return description;
     }
-    
+
 }
---
+++ src/main/java/de/uni_hamburg/corpora/visualization/resources/vikus-viewer/info.md
@@ -0,0 +1,18 @@
+# INEL Dolgan
+
+The INEL Dolgan corpus has been created within the long-term [INEL project](https://inel.corpora.uni-hamburg.de/) - "Grammatical Descriptions, Corpora and Language Technology for Indigenous Northern Eurasian Languages", 2016–2033. The corpus makes possible typologically aware corpus-based grammatical research on the Dolgan language and expands the documentation of the lesser described indigenous languages of Northern Eurasia.
+
+Dolgan is an endangered Siberian (North-Eastern) Turkic language, which is spoken on the Taymyr peninsula and in adjacent areas in Northern Siberia. The number of speakers is approx. 1,000. Whereas in many settlements, including Dudinka, Dolgan is rarely spoken in everyday's life, there are three remote villages (Syndassko, Popigay and Novorybnoe) where Dolgan is until now very present and frequently spoken. 
+
+![tags](img/infobar_tags.svg)
+
+The keywords represent topics and the region of the manuscripts. Click on the keywords in order to thematically narrow down the selection of manuscripts.
+
+![time](img/infobar_time.svg)
+
+The manuscripts are positioned along their year of origin. Below the year you can find further information.
+
+![time](img/infobar_scroll.svg)
+
+You can either zoom with your touchpad or scroll wheel in the areas of the manuscripts in order to see more details. Klick and drag the background in order to adjust the visible area.
+
