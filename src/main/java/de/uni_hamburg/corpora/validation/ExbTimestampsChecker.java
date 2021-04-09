d547bd40993ddb5779c306b80afd1bbebc0835ba
Aleksandr Riaposov <bay7303@uni-hamburg.de>
2021-04-09 10:41:30 +0200

    Updated timestamps checker

--- src/main/java/de/uni_hamburg/corpora/validation/ExbTimestampsChecker.java
+++ src/main/java/de/uni_hamburg/corpora/validation/ExbTimestampsChecker.java
@@ -12,7 +12,6 @@ import de.uni_hamburg.corpora.CorpusIO;
 import de.uni_hamburg.corpora.Report;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.regex.Pattern;
 import org.jdom.Document;
 import org.jdom.JDOMException;
 import org.jdom.xpath.XPath;
@@ -46,30 +45,51 @@ public class ExbTimestampsChecker extends Checker implements CorpusFunction {
         
     /**
      * One of the main functionalities of the feature; issues warnings if the
-     * exb file has missing timestamps in the common timeline.  
+     * exs file has missing timestamps at the beginnings or ends of a segment chain.  
      */
     @Override
     public Report function(CorpusData cd, Boolean fix) // check whether there's any illegal apostrophes '
             throws SAXException, IOException, ParserConfigurationException, URISyntaxException, JDOMException, TransformerException, XPathExpressionException {
         Report stats = new Report();         // create a new report
         doc = TypeConverter.String2JdomDocument(cd.toSaveableString()); 
-        String xpathTimeline = "//tli";
-        Pattern tliSegmentPattern = Pattern.compile("^T\\d+$"); 
-        XPath timeline = XPath.newInstance(xpathTimeline);
-        List allTlis = timeline.selectNodes(doc);
+        String xpathSegment = "//segmentation/ts";
+        XPath segment = XPath.newInstance(xpathSegment);
+        List allSegments = segment.selectNodes(doc);
         CorpusIO cio = new CorpusIO();     
-        for (int i = 0; i < allTlis.size(); i++) {
-            Object o = allTlis.get(i);
+        for (int i = 0; i < allSegments.size(); i++) {
+            Object o = allSegments.get(i);
             if (o instanceof Element) {
                 Element e = (Element) o;
-                String tli = e.getAttributeValue("id");
-                String tliTimestamp = e.getAttributeValue("time");
-                if (tliTimestamp == null) {
-                    if (tliSegmentPattern.matcher(tli).find()) {
+                String start = e.getAttributeValue("s");
+                String end = e.getAttributeValue("e");
+                String xpathStart = "//tli[@id='" + start + "']";
+                XPath timelineStart = XPath.newInstance(xpathStart);
+                List tliStart = timelineStart.selectNodes(doc);
+                Object sTli = tliStart.get(0);
+                if (sTli instanceof Element) {
+                    Element el = (Element) sTli;
+                    String id = el.getAttributeValue("id");
+                    String time = el.getAttributeValue("time");
+                    if (time == null) {
                         missingTimestamp = true;
-                        String message = "Missing timestamp at " + tli;
-                        exmaError.addError(function, cd.getURL().getFile(), "", tli, false, message);
-                        stats.addCorrect(function, cd, message);
+                        String message = "Missing timestamp at the start of the segment chain at " + id;
+                        exmaError.addError(function, cd.getURL().getFile(), "", id, false, message);
+                        stats.addWarning(function, cd, message);
+                    }
+                }
+                String xpathEnd = "//tli[@id='" + end + "']";
+                XPath timelineEnd = XPath.newInstance(xpathEnd);
+                List tliEnd = timelineEnd.selectNodes(doc);
+                Object eTli = tliEnd.get(0);
+                if (eTli instanceof Element) {
+                    Element el = (Element) eTli;
+                    String id = el.getAttributeValue("id");
+                    String time = el.getAttributeValue("time");
+                    if (time == null) {
+                        missingTimestamp = true;
+                        String message = "Missing timestamp at the end of the segment chain at " + id;
+                        exmaError.addError(function, cd.getURL().getFile(), "", id, false, message);
+                        stats.addWarning(function, cd, message);
                     }
                 }
             }
@@ -88,7 +108,7 @@ public class ExbTimestampsChecker extends Checker implements CorpusFunction {
     @Override
     public Collection<Class<? extends CorpusData>> getIsUsableFor() {
         try {
-            Class cl = Class.forName("de.uni_hamburg.corpora.BasicTranscriptionData");
+            Class cl = Class.forName("de.uni_hamburg.corpora.SegmentedTranscriptionData");
             IsUsableFor.add(cl);
         } catch (ClassNotFoundException ex) {
             report.addException(ex, "Usable class not found.");
@@ -103,7 +123,7 @@ public class ExbTimestampsChecker extends Checker implements CorpusFunction {
     @Override
     public String getDescription() {
         String description = "This class issues warnings if it finds missing "
-                + "timestamps in the timeline";
+                + "timestamps in the timeline at the beginning or end of a segment chein";
         return description;
     }
 
